04014e6fa7a33f04e595fca6bd30777f4e406e38
Kweeme <76181396+Kweeme@users.noreply.github.com>
2022-05-02 10:47:37 -0400

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -11,16 +11,6 @@ module.exports = async (client) => {
         let statuses = [{name: 'for /invites', type: 'WATCHING'}, {name: `${client.guilds.cache.size} servers`, type: 'WATCHING'}];
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
   		client.user.setActivity(status.name, {type: status.type, url: 'https://twitch.tv/twitch'});
-        await axios({
-        url: `https://discord.boats/api/bot/${client.user.id}`,
-        method: 'POST',
-        headers: {
-            Authorization: process.env['dbToken']
-        },
-        data: {
-            server_count: client.guilds.cache.size
-        }
-        }).then(async (res) => {if (res.data.error) {console.log(res.data.message)}})
   	}, 10000)
     
    
